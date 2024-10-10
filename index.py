0c2601098936d287aa4a3e1b45b15f4b47faa5b2
nathaliavsmarques <nathaliavsmarques@outlook.com>
2024-10-10 16:58:04 -0300

    organização na parte do código referente a transferir dinheiro

--- index.py
+++ index.py
@@ -16,14 +16,12 @@ saldo = 0
 while True:
     opcao = input('Digite Opção: ').upper()
     if opcao =='A':
-        print(f'Seu saldo é de R$ {saldo:.2f}')
-        
+        print(f'Seu saldo é de R$ {saldo:.2f}')        
     elif opcao =='B':
         deposito = float(input('Informe o valor do depósito: R$  '))
         saldo += deposito
         print(f'Você depositou R$ {deposito:.2f}.')
-        print(f'Seu saldo agora é de: R$ {saldo:.2f}')
-        
+        print(f'Seu saldo agora é de: R$ {saldo:.2f}')        
     elif opcao =='C':
         saque = float(input('Informe o valor do saque: R$ '))
         if saque > saldo :
@@ -31,32 +29,23 @@ while True:
         else: 
             saldo -= saque
             print(f'Você sacou R$ {saque:.2f}. \nSeu saldo agora é de: R$ {saldo:.2f}')
-        print(f'')
-        
+        print(f'')        
     elif opcao =='D':
         cpf = input('Informe o CPF do destinatário: ')
-        if len(cpf) == 11: 
-            transferir = float(input('Informe o valor que deseja transferir: R$ '))
-            saldo -= transferir
-            print(f'Você transferiu R$ {transferir:.2f} para o CPF {cpf}.\nSeu saldo agora é de R$ {saldo:.2f}')
-        elif len(cpf) != 11:
-            print(f'ATENÇÃO, o CPF deve conter 11 dígitos.')
-        
-        
-        #PARTE QUE FALTA
-        '''elif cpf is not int:
-            print('ATENÇÃO! Por favor, informe somente valores numéricos.')       
-        
+        tratando_cpf = cpf.replace('.', '').replace('-', '')
+        if len(tratando_cpf) != 11:
+            print('O CPF deve conter 11 dígitos numéricos.')
         else:
             transferir = float(input('Informe o valor que deseja transferir: R$ '))
-            saldo -= transferir
-            print(f'Você transferiu R$ {transferir:.2f} para o CPF {cpf}. end=''. Seu saldo agora é de R$ {saldo:.2f}')  '''          
-    
+            if saldo < transferir:
+                print('Erro, saldo insuficiente.')
+            else: 
+                saldo -= transferir
+                print(f'Você transferiu R$ {transferir:.2f} para o CPF {cpf}.\nSeu saldo agora é de R$ {saldo:.2f}')        
     elif opcao =='E':
         print('Você escolheu letra E')
     else:
-        print('Você escolheu uma opção inválida')
-    
+        print('Você escolheu uma opção inválida')    
     
     continuar = input('Deseja fazer outra operação ?: [S/N] ').upper()
     if continuar == 'N':
@@ -64,6 +53,3 @@ while True:
         print('OPERAÇÃO ENCERRADA')
         print('-*' * 10)       
         break   
-    
-
-    
